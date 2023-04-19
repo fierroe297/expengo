1ffb12bc922e79ea3aaa654e7fb3a12ec534d49e
Emmanuel Fierro <60017920+fierroe297@users.noreply.github.com>
2023-04-19 00:16:06 -0600

    Test of curl to implement instagram token

---
+++ src/pages/AboutPage.page.js
@@ -0,0 +1,18 @@
+https://api.instagram.com/oauth/authorize
+  ?client_id=559024046348768
+  &redirect_uri=https://expengo.vercel.app/login?redirectTo=%2F
+  &scope=user_profile,user_media
+  &response_type=code
+
+  curl -X POST \
+  https://api.instagram.com/oauth/access_token \
+  -F client_id=913531276645979 \
+  -F client_secret=b2d2feea88f067a2f2b804486d65627f \
+  -F grant_type=authorization_code \
+  -F redirect_uri=https://expengo.vercel.app/login? \
+  -F code=AQB56JI_E8nKQmViDRu_Js3FBXY0RVK8Wy1xpSepop8c_aCSOsUJnSQCCmU8XAnJ0dIVrMRUB5TSQSUaUzUz4Cwk5MtE5vNszUQi1dc0XiViXzsV4kC7SPTyQE3794UNd3JsIOb1aMX06BoKytC57xNqnHprucGxvwyDpqYCAkeV2qtZySjQlx4wMB_wp7ZMpeWgownuEc1dx8-KaLxTKxQJl3jZVNyQjKGaE6oIaB7-JA
+  
+  
+  //https://expengo.vercel.app/login?code=AQB56JI_E8nKQmViDRu_Js3FBXY0RVK8Wy1xpSepop8c_aCSOsUJnSQCCmU8XAnJ0dIVrMRUB5TSQSUaUzUz4Cwk5MtE5vNszUQi1dc0XiViXzsV4kC7SPTyQE3794UNd3JsIOb1aMX06BoKytC57xNqnHprucGxvwyDpqYCAkeV2qtZySjQlx4wMB_wp7ZMpeWgownuEc1dx8-KaLxTKxQJl3jZVNyQjKGaE6oIaB7-JA
+
+  https://expengo.vercel.app/login?code=AQA2AZtrNwEVQCGiceuKtCvmBz0iVXL68HpXpWjo-TTbLiFzWWcS_m_wABKC6yqrov0ASFj3QMZgIg4wyx4bqapWhwD1dmFAsrjF8LLybaCK2EG-BReqDgZAR2gOd_t48rNfsdcu2y8VSzhAlc0Db82ctyjjn4E-UxI7QDVLK1kX30WVkAxCplqthWh5XztRKHSKCScKzOT_ADUMDgTBKyWHjV2SJ_EyDiEDoav5-E5xzw#_
